1c898845589d00cc7368c0dde161c86f0b7819aa
gerardo-ramirez <gerardo.ramirez@xa.epicgames.com>
2025-09-25 10:35:37 -0600

    Add Footer component

--- eazystore-ui/src/App.jsx
+++ eazystore-ui/src/App.jsx
@@ -1,7 +1,14 @@
 import Header from "./components/Header";
+import Footer from "./components/footer/Footer";
+import React from "react";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
